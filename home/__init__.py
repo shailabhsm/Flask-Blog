81f1c9bcd2bed0b270afcaab36fd50c7cb0b31f4
Shailabh Suman <shailabh.sm131128@gmail.com>
2021-02-17 11:36:43 +0530

    first commit

---
+++ home/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '2bd483c0803ba997c45ba7481ed2d28b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from home import routes
